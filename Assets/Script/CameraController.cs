e0206a1aa92de8fcec59876bbe07b342016bf54e
Non-Afk <71341604+Non-Afk@users.noreply.github.com>
2021-02-08 00:03:38 -0500

    Add player movement and animation

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector2 Margin;
+    public Vector2 smoothing;
+    public BoxCollider2D Bounds;
+    private Vector3 _min;
+    private Vector3 _max;
+    public bool IsFollowing { get; set; }
+
+    void Start()
+    {
+        _min = Bounds.bounds.min;
+        _max = Bounds.bounds.max;
+        IsFollowing = true;
+    }
+
+    void Update()
+    {
+        var x = transform.position.x;
+        var y = transform.position.y;
+        if (IsFollowing)
+        {
+            if (Mathf.Abs(x - player.position.x) > Margin.x)
+            {//如果相机与角色的x轴距离超过了最大范围则将x平滑的移动到目标点的x
+                x = Mathf.Lerp(x, player.position.x, smoothing.x * Time.deltaTime);
+            }
+        }
+        float orthographicSize = GetComponent<Camera>().orthographicSize;//orthographicSize代表相机(或者称为游戏视窗)竖直方向一半的范围大小,且不随屏幕分辨率变化(水平方向会变)
+        var cameraHalfWidth = orthographicSize * ((float)Screen.width / Screen.height);//的到视窗水平方向一半的大小
+        x = Mathf.Clamp(x, _min.x + cameraHalfWidth, _max.x - cameraHalfWidth);//限定x值
+        transform.position = new Vector3(x, y, transform.position.z);//改变相机的位置
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    public int rotation = 1;
+    public Animator animator;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.A) && rotation > 0)
+        {
+            transform.Rotate(new Vector3(0, 180, 0));
+            rotation = -1;
+        }
+        if (Input.GetKeyDown(KeyCode.D) && rotation < 0)
+        {
+            transform.Rotate(new Vector3(0, 180, 0));
+            rotation = 1;
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            animator.SetFloat("Player_speed", 0);
+            animator.SetBool("Fight", true);
+
+        }
+        else
+        {
+            animator.SetBool("Fight", false);
+            transform.position += new Vector3(movement, 0, 0);// * Time.deltaTime * MovementSpeed;
+            animator.SetFloat("Player_speed", Mathf.Abs(movement));
+
+        }
+    }
+}
